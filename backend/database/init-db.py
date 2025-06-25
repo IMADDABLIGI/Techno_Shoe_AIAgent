3e349b0e959838ae801f6291d1abe65839ef9cbd
IMADDABLIGI <imad.dabligi@gmail.com>
2025-06-25 12:18:44 +0100

    refactor(master): Add brands images to init database file

--- backend/database/init-db.py
+++ backend/database/init-db.py
@@ -8,6 +8,13 @@ from datetime import datetime
 # Load environment variables
 load_dotenv()
 
+adidas = "https://assets.adidas.com/images/w_1880,f_auto,q_auto/10d27f0989844a15bdae4946ff002c65_9366/JI2307_04_standard.jpg"
+new_balance = "https://nb.scene7.com/is/image/NB/m2002rcc_nb_05_i?$pdpflexf2$&qlt=80&fmt=webp&wid=440&hei=440"
+nike = "https://ma.mojaa.com/cdn/shop/files/FD6454-104_1_575x.jpg?v=1750331146"
+# puma = "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_550,h_550/global/395205/78/fnd/EEA/fmt/png/Sneakers-Suede-XL-Unisexe"
+puma = "https://images.puma.com/image/upload/f_auto,q_auto,b_rgb:fafafa,w_550,h_550/global/395205/78/sv04/fnd/EEA/fmt/png/Sneakers-Suede-XL-Unisexe"
+reebok = "https://www.reebok.eu/cdn/shop/files/22253063_52160247_800.webp?v=1744648695&width=800"
+
 def get_db_connection():
     """Establish connection to MongoDB and return database object"""
     client = MongoClient(os.getenv("CONNECTION_STRING"))
@@ -28,12 +35,18 @@ def generate_simple_shoes_data() -> List[Dict]:
     categories = ["Running", "Basketball", "Casual", "Training"]
     colors = ["Black", "White", "Red", "Blue", "Gray"]
     genders = ["Men", "Women", "Unisex"]
-    # European sizes for adults (36-47)
     sizes = list(range(36, 48))
+    
+    brand_images = {
+        "Nike": nike,
+        "Adidas": adidas,
+        "Puma": puma,
+        "Reebok": reebok,
+        "New Balance": new_balance
+    }
     
     shoes_data = []
     
-    # Generate 100 shoes
     for i in range(100):
         brand = random.choice(brands)
         category = random.choice(categories)
@@ -42,13 +55,13 @@ def generate_simple_shoes_data() -> List[Dict]:
             "name": f"{brand} {category} {i+1}",
             "brand": brand,
             "category": category,
-            "price": round(random.uniform(400, 1000), 2),  # Prices in DH
+            "price": round(random.uniform(400, 1000), 2),
             "color": random.choice(colors),
             "sizes": random.sample(sizes, random.randint(3, 5)),
             "gender": random.choice(genders),
             "rating": round(random.uniform(3.5, 5.0), 1),
-            "in_stock": True
-            # "in_stock": random.choice([True, False])
+            "in_stock": True,
+            "image": brand_images[brand]
         }
         
         shoes_data.append(shoe)
