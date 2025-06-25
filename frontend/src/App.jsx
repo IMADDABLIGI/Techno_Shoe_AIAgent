6d696cc70b69046cd21bd36e3733975016316092
IMADDABLIGI <imad.dabligi@gmail.com>
2025-06-25 18:20:12 +0100

    refactor(master/frontend): fixing apis connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Send, Bot, User, ShoppingBag, Star, Heart, ShoppingCart } from 'lucide-
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:5000/api';
-
 const API_URL = import.meta.env.VITE_API_URL;
 
 const ShoeCard = ({ shoe, onAddToCart }) => {
